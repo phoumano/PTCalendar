a1d88b2a4ab38081823fb764f6ca47fdc35271fb
Phoumano Thongsithavong <>
2018-07-20 23:09:07 -0700

    Moved testing frameworks into test target for PTCalendar

---
+++ airbnbcalendar/ptcalendarTests/CalendarDayCollectionViewCellSpec.swift
@@ -0,0 +1,11 @@
+//
+//  CalendarDayCollectionViewCellSpec.swift
+//  ptcalendarTests
+//
+//  Created by PHOUMANO THONGSITHAVONG on 7/20/18.
+//  Copyright © 2018 Phoumano Thongsithavong. All rights reserved.
+//
+
+import Foundation
+import Quick
+import Nimble
